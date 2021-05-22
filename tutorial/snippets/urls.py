cfc0062f436834cc6027a84de9ae74ac94276ce2
Гусейн <manblax@yandex.ru>
2021-05-22 19:55:57 +0300

    Переместить файлы

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from snippets import views
+from tut.snippets import views
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,9 +1,8 @@
-from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics
+from tut.snippets.models import Snippet
+from tut.snippets.serializers import SnippetSerializer, UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import permissions
-from snippets.permissions import IsOwnerOrReadOnly
+from tut.snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
